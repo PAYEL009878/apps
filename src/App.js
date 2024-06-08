6ae011b06065868e5013a95d65e7b905c39fc810
Payel009878 <mannapihu560@gmail.com>
2024-06-08 16:56:05 +0530

    react-with-js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,62 @@ for (let item of people)
     console.log(person.age)
   });
   */
+
+ // for in
+
+ /*const employee ={
+  name:"Payel",
+  age:20,
+  occupation:"engineer",
+ };
+ for(const key in employee){
+  if(employee.hasOwnProprty(key)){
+    console.log('${key}: ${employee[key]}')
+  }
+ }
+ //also for concatenate
+
+ console.log()*/
+/*for filtering data
+
+const products = [
+(id: 1001, name: "Laptop", price: 1000, category: "Electronics"),
+ ( id: 1002, name: "shirt", price: 50, category: "clothing"},
+(id: 1003, name: "coffee maker", price: 88, category: "Home Appliance"),
+ ( id: 1004, name: "Headphones", price: 200, category: "Electronics"),
+(id: 1005, name: "pants", price: 60, category: "clothing"},
+];
+
+
+const filteredProducts = products.filter((item) => {
+  return item.category ==="clothing";
+));
+console.log("filteredProducts", filtered Products);
+// find method
+const foundProducts = products.find((item) =>{
+return item.id === 1002;
+ });
+
+console.log(foundProducts);
+*/
+/* map keyword for returning value
+
+const employees = [
+(name: "Niraj", age: 22, salary:50), "Niraj": Unknown word.
+(name: "Rudra", age: 30, salary:80),  "Rudra": Unknown word.
+(name: "roy" age: 30, salary:100},
+];
+const mappedEmployees= employees.map((item) =>{
+console.log("item",item);
+return {
+...item,
+salary: item.salary + 100,
+ };
+
+}, []);
+console.log("employees", employees);
+console.log("mapped Employees", mapped Employees);
+*/
     return (
     <div className="App">
       <header className="App-header">
