2f5b1dce0022b474ea192c6bf387d349715e67d0
Payel009878 <mannapihu560@gmail.com>
2024-06-11 17:33:51 +0530

    useeffect-with-dependency

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ import { useEffect,useState } from 'react';
 import './App.css'
 
 function App() {
-  const [data, setdata] = useState<string>();
+  const [data,setData]=useState<number>(0);
+  const [numberData, setNumberData] = useState<number>(0);
 useEffect(()=>{
-  setdata("world");
-},[]);
+  addData();
+},[numberData]);
+const addData=()=>{
+  const oldData=data;
+  const newData=oldData+1;
+setData( newData);
+console.log(newData);
+};
+const decrementData=()=>{
+  setNumberData(numberData-1);
+};
+  
   return (
     <>
-      <div>{data}
-      </div>
-
+    <button title ="title" onClick={addData} className={'number ${data'}>add data</button>
+      <div> Incremented data: {data}</div>
+      <br />
+      <button onClick={decrementData} className={'decrement ${numberData}'}>decrement</button>
+      <div>decrement data: {numberData}</div>
     </>
   );
 }
