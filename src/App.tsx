583a27020c261f8656e46ff19a6d3556543d6b97
Payel009878 <mannapihu560@gmail.com>
2024-06-12 21:29:17 +0530

    useEffect-with-dependancy

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,42 @@
-import { useEffect,useState } from 'react';
+import {useEffect,useState } from 'react';
 import './App.css'
 
 function App() {
   const [data,setData]=useState<number>(0);
   const [numberData, setNumberData] = useState<number>(0);
-useEffect(()=>{
-  addData();
-},[numberData]);
-const addData=()=>{
-  const oldData=data;
-  const newData=oldData+1;
-setData( newData);
-console.log(newData);
+  const numberArray=[1,2,3,4,5,6,7,8];
+  const people=[
+    {name:"john" ,age:33 },
+    {name:"riya" ,age:33 },
+    {name:"lia" ,age:33 },];
+useEffect(()=>{},
+[numberData]);
+const addData =()=>{
+
 };
 const decrementData=()=>{
-  setNumberData(numberData-1);
 };
   
   return (
     <>
-    <button title ="title" onClick={addData} className={'number ${data'}>add data</button>
+    <button title ="title" onClick={addData} className={'number ${data'}>   </button>
       <div> Incremented data: {data}</div>
       <br />
-      <button onClick={decrementData} className={'decrement ${numberData}'}>decrement</button>
-      <div>decrement data: {numberData}</div>
-    </>
+      <button onClick={decrementData} className={'decrement ${numberData}'}>   </button>
+      <div>decremented data: {numberData}</div>
+    {numberArray.map((item,i)=>{
+    })}
+    {people.map((item,i)=>{
+  return (
+    <div key={i}>
+          <div> name: {item.name}</div>
+          <div> Age: {item.age}</div>
+          <br />
+          </div>
+         );
+    })}  
+      </>
+
   );
 }
 
